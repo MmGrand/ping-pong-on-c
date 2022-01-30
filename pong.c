45e0689512cc725b4dd2e61782d70b82d45a88b2
MmGrand <maksimzarubin20002000@gmail.com>
2022-01-30 21:56:30 +0700

    finish_version

--- pong.c
+++ pong.c
@@ -3,20 +3,20 @@
 
 const int x_min = 0, y_min = 0, x_max = 80, y_max = 25;
 
-void create_field(int x_min, int y_min, int x_max, int y_max, int x_p1, int y_p1, int x_p2, int y_p2, int x_ball, int y_ball, int count_p1, int count_p2);
+void play(int x_min, int y_min, int x_max, int y_max, int x_p1, int y_p1, int x_p2, int y_p2, int x_ball, int y_ball, int count_p1, int count_p2);
 int Count_the_key();
 
 int main() {
     int x_p1 = 1, y_p1 = 13, x_p2 = 78, y_p2 = 13, x_ball = (x_max - 1 - x_min) / 2, y_ball = 13;
     int count_p1 = 0, count_p2 = 0;
-    create_field(x_min, y_min, x_max, y_max, x_p1, y_p1, x_p2, y_p2, x_ball, y_ball, count_p1, count_p2);
+    play(x_min, y_min, x_max, y_max, x_p1, y_p1, x_p2, y_p2, x_ball, y_ball, count_p1, count_p2);
 }
 
-void create_field(int x_min, int y_min, int x_max, int y_max, int x_p1, int y_p1, int x_p2, int y_p2, int x_ball, int y_ball, int count_p1, int count_p2) {
+void play(int x_min, int y_min, int x_max, int y_max, int x_p1, int y_p1, int x_p2, int y_p2, int x_ball, int y_ball, int count_p1, int count_p2) {
     char key;
     int k = 0;
     bool vert = false, goriz = true;
-    while (count_p1 != 1 && count_p2 != 1) {
+    while (count_p1 != 21 && count_p2 != 21) {
         for (int i = y_max; i > y_min; i--) {
         for (int j = x_min; j < x_max; j++) {
             if (i == y_max && j == x_min) {
@@ -69,7 +69,6 @@ void create_field(int x_min, int y_min, int x_max, int y_max, int x_p1, int y_p1
                 y_p2 += 1;
             }
         }
-        //k++;
         if ((y_ball == y_p1 && x_ball - 1 == x_p1) || (y_ball == y_p1 + 1 && x_ball - 1 == x_p1) || (y_ball == y_p1 - 1 && x_ball -+ 1 == x_p1)) {
             if (vert == true) {
                 vert = false;
